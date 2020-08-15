e6259e8da4085835ce47819c670a43dfcafa1619
nakao107107 <imakeanewstory@gmail.com>
2020-08-16 08:52:55 +0900

    tmp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,5 @@ class ApplicationController < ActionController::API
   def check_session
     access_token = request.headers[:Authorization]
     @mentor = Mentor.where(access_token: access_token).first
-  rescue StandardError => e
-    render json: { error: e.message }, status: 401
   end
 end
