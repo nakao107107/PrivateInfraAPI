90a68c1194a658e0d661eae751cce774a4c51c54
nakao107107 <imakeanewstory@gmail.com>
2020-10-01 20:10:26 +0900

    イベント更新メソッドの作成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -18,6 +18,14 @@ class EventsController < ApplicationController
     render json: event, serializer: EventSerializer
   end
 
+  def update
+    event = Event.find(params[:id])
+    event.update!(event_params)
+    event.relate_industries(params[:industries])
+    event.relate_occupations(params[:occupations])
+    render json: event, serializer: EventSerializer
+  end
+
   def progress
     event_ids = Event.where("deadline >= ?", Time.current).all.pluck(:id)
     progress_event_ids = Progress.where(mentor_id: @mentor.id).where(status: 'IN_PROGRESS').pluck(:event_id)
